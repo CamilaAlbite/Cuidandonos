724aa83224f711ce780e8d0012816dd51866c3b6
Nahuel <nfleitas@frba.utn.edu.ar>
2024-05-01 18:44:20 -0300

    Agrego calculadora para distancias y tiempo (No tiene conexion con la api)

---
+++ src/main/java/ar/edu/utn/frba/dds/CalculadoraDeDistancias.java
@@ -0,0 +1,19 @@
+package ar.edu.utn.frba.dds;
+
+public class CalculadoraDeDistancias {
+	public CalculadoraDeDistancias() {
+		
+	}
+	
+	public Integer calcularTiempoAproximado(String origen, String destino) {
+		//No tenemos conexion con la API, ponemos un valor de ejemplo
+		//Tiempo en minutos
+		return 60;
+	}
+	
+	public Integer calcularDistancia(String origen, String destino) {
+		//No tenemos conexion con la API, ponemos un valor de ejemplo
+		//Distancia en metros
+		return 1000;
+	}
+}
