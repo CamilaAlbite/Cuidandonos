4f8e9630ab3874a04f77304c2cb0600e9449df13
Nahuel <nfleitas@frba.utn.edu.ar>
2024-05-01 18:45:34 -0300

    Agrego mensajes

--- src/main/java/ar/edu/utn/frba/dds/TipoMensaje.java
+++ src/main/java/ar/edu/utn/frba/dds/TipoMensaje.java
@@ -3,5 +3,11 @@ package ar.edu.utn.frba.dds;
 public enum TipoMensaje {
     ALERTA,
     ARRIBO,
-    INICIO_DE_VIAJE
+    INICIO_DE_VIAJE,
+    SOLICITUD_CUIDADOR,
+    SOLICITUD_ACEPTADA,
+    SOLICITUD_RECHAZADA,
+    MINUTOS,
+    DISTANCIA,
+    SIN_PELIGRO
 }
